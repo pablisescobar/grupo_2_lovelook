fe26d6c81e4486c159da035575791f823d647391
Emanuel Arroyo <ema201217@gmail.com>
2021-08-30 18:49:52 -0300

    arreglo la falla de las categorias en el header

--- servidor express/src/controllers/infoController.js
+++ servidor express/src/controllers/infoController.js
@@ -9,19 +9,19 @@ module.exports =  {
     contact: (req,res)=>{
         res.render('info/contact',{
             position:"",
-            categorias:categorias
+            categorias
         })
-    },
+        },
     help: (req,res)=>{
         res.render('info/help',{
             position:"",
-            categorias:categorias
+            categorias
         })
     },
     we: (req,res)=>{
         res.render('info/we',{
             position:"position:relative",
-            categorias:categorias
+            categorias
         })
     }
 }
