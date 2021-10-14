c46e5b657d193986faf079c2ea8971454ad95d41
unknown <ema201217@gmail.com>
2021-10-13 22:22:46 -0300

    directorio data modificado

--- servidor express/src/data/dataBase.js
+++ servidor express/src/data/dataBase.js
@@ -2,22 +2,5 @@ const fs=require('fs')
 const path = require('path')
 
 module.exports = {
-    getShops : JSON.parse(fs.readFileSync('./src/data/shops.json','utf-8')),
-
-    getProducts:JSON.parse(fs.readFileSync('./src/data/products.json','utf-8')),
-    
     getImgCarousel:JSON.parse(fs.readFileSync('./src/data/carousel.json','utf-8')),
-
-    getCategory:JSON.parse(fs.readFileSync('./src/data/category.json','utf-8')),
-
-    getUsers:JSON.parse(fs.readFileSync('./src/data/users.json', 'utf-8')),
-  
-    writeProductsJSON: (dataBase) => {
-        fs.writeFileSync(path.join(__dirname, "../data/products.json"), JSON.stringify(dataBase), "utf-8")
-    },
-
-    writeUsersJSON: (dataBase) => {
-        fs.writeFileSync(path.join(__dirname, "../data/users.json"), JSON.stringify(dataBase), "utf-8")
-    }
-
 }
