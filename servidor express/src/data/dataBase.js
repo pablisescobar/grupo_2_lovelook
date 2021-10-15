6aa47de187cf00a861976a8fcf3b9fe55d722901
Mariela <marielapaduani@gmail.com>
2021-10-15 19:17:10 -0300

    Add Changes

--- servidor express/src/data/dataBase.js
+++ servidor express/src/data/dataBase.js
@@ -2,7 +2,5 @@ const fs=require('fs')
 const path = require('path')
 
 module.exports = {
-    getImgCarousel:JSON.parse(fs.readFileSync('./src/data/carousel.json','utf-8')),
-    getProducts:JSON.parse(fs.readFileSync('./src/data/products.json','utf-8')),
-    getImgCarousel:JSON.parse(fs.readFileSync('./src/data/carousel.json','utf-8')),
+    getImgCarousel:JSON.parse(fs.readFileSync('./src/data/carousel.json','utf-8'))
 }
