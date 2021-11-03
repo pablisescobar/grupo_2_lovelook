6b04481d9d268d7a16ce36aeb21bb91e8a509946
Mariela <marielapaduani@gmail.com>
2021-11-03 01:35:37 -0300

    Add validations FrontEnd

--- servidor express/src/validations/adminValidation.js
+++ servidor express/src/validations/adminValidation.js
@@ -3,40 +3,42 @@ let { check } = require("express-validator");
 module.exports = [
   check("name")
     .notEmpty()
-    .withMessage("Ingrese un nombre para el producto")
-    .bail()
+    .withMessage("El campo nombre es obligatorio").bail()
+    .isAlpha('es-ES')
+    .withMessage('Debes ingresar un nombre valido')
     .isLength({ min: 5 })
     .withMessage("El nombre debe ser mayor a 5 caracteres"),
 
   check("description")
     .notEmpty()
     .withMessage("Debes ingresar una descripción")
+    .isAlpha('es-ES')
+    .withMessage('Debes ingresar una descripción válida')
     .isLength({ min: 20 })
     .withMessage("Debes ingresar mínimo 20 caracteres"),
 
-  check("season").notEmpty()
+  check("season")
+    .notEmpty()
     .withMessage("Selecciona una temporada"),
 
   check("category").notEmpty()
-    .withMessage("Selecciona una categoria"),
+    .withMessage("Selecciona una categoría"),
 
   check("price")
     .notEmpty()
-    .withMessage("Ingresa un precio")
-    .bail()
+    .withMessage("Ingresa un precio").bail()
     .isNumeric()
     .withMessage("Ingresa un valor numerico"),
 
   check("color").notEmpty()
-    .withMessage("Selecciona o agrega un color").bail(),
+    .withMessage("Selecciona un color").bail(),
 
   check("size").notEmpty()
     .withMessage("Selecciona un talle"),
 
   check("amount")
     .notEmpty()
-    .withMessage("Ingresa una cantidad")
-    .bail()
+    .withMessage("Ingresa una cantidad").bail()
     .isNumeric()
     .withMessage("Ingresa un valor númerico"),
 ];
--- servidor express/src/validations/insertColor.js
+++ servidor express/src/validations/insertColor.js
@@ -3,7 +3,5 @@ let { check } = require('express-validator')
 module.exports = [
     check('colorInsert')
         .notEmpty()
-        .withMessage('Ingresa un valor').bail()
-        .isAlphanumeric()
-        .withMessage('Ingresa una cadena de texto'),
+        .withMessage('Ingrese una color').bail()
 ]
