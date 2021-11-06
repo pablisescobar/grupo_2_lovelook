39ce9a8e66a17d0f0759fa2cd011d4a22b0e26f4
paloescobar29 <pablodescobarocampo@gmail.coml.com>
2021-11-05 23:08:09 -0300

    cambios en el header

---
+++ servidor express/public/js/profileEdit.js
@@ -0,0 +1,12 @@
+function global(element){
+    return document.getElementById(element)
+}
+
+window.addEventListener('load', ()=> {
+    let name = global('nombre'),
+    last_name = global('apellido'),
+    email = global('email'),
+    phone = global('telefono'),
+    address = global(' direccion'),
+    codepostal
+})
