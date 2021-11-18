2e473be4aad4cfbe84b2e5917757cc1c7c8cc1e9
paloescobar29 <pablodescobarocampo@gmail.coml.com>
2021-11-18 01:16:27 -0300

    delete entre otras

--- servidor express/src/controllers/userController.js
+++ servidor express/src/controllers/userController.js
@@ -124,8 +124,113 @@ module.exports = {
           }
           res.redirect('/')
         })
- })
-  },
+      })
+    },
+
+    updateProfile: (req, res) => {
+        let errors = validationResult(req)
+
+        if (errors.isEmpty()) {
+            let { firstname, lastName, phone, address, pc, province, city } = req.body;
+            db.User.update({
+                firstname,
+                lastName,
+                pc,
+                phone,
+                avatar: req.file ? req.file.filename : req.session.user.avatar
+            }, {
+                where: {
+                    id: req.params.id
+                }
+            })
+                .then(() => {
+                    db.Location.create({
+                        address,
+                        city,
+                        province,
+                        pc,
+                        userId: req.params.id
+                    })
+                        .then(() => {
+                            res.redirect('/user/perfil')
+                        })
+                })
+
+        } else {
+            res.render('users/userProfileEdit', {
+                position: "position:relative;",
+                errors: errors.mapped(),
+                old: req.body,
+                session: req.session
+            })
+        }
+    },
+
+    processLogin: (req, res) => {
+        let errors = validationResult(req)
+        if (errors.isEmpty()) {
+            db.User.findOne({
+                where: {
+                    email: req.body.email
+                }
+            })
+                .then(user => {
+                    req.session.user = {
+                        id: user.id,
+                        firstName: user.firstName,
+                        lastName: user.lastName,
+                        email: user.email,
+                        rol: user.rolId
+                    };
+                    if (req.body.remember) {
+                        res.cookie("userLoveLook", req.session.user, { expires: new Date(Date.now() + 90000), httpOnly: true })
+                    };
+                    res.locals.user = req.session.user
+
+                    res.redirect('/')
+                })
+        } else {
+            res.render('users/login', {
+                position: "position: relative;",
+                errors: errors.mapped(),
+                old: req.body,
+                session: req.session
+            })
+        }
+    },
+    processRegister: (req, res) => {
+        let errors = validationResult(req)
+
+        if (errors.isEmpty()) {
+
+            let { firstName, lastName, email, password } = req.body
+            db.User.create({
+                firstName,
+                lastName,
+                email,
+                password: bcrypt.hashSync(password, 12),
+                rolId: 1,
+                avatar: 'default-image.png'
+            }).then(() => {
+                res.redirect('/user/login')
+            }).catch(err => console.log(err))
+        } else {
+            res.render('users/register', {
+                errors: errors.mapped(),
+                old: req.body,
+                session: req.session,
+                position: "position: relative"
+            });
+        }
+    },
+
+    logout: (req, res) => {
+        req.session.destroy();
+        if (req.cookies.userLoveLook) {
+            res.cookie('userLoveLook', '', { maxAge: -1 })
+          }
+          res.redirect('/')
+        },
 
   processLogin: (req, res) => {
     let errors = validationResult(req)
@@ -152,10 +257,51 @@ module.exports = {
         res.locals.user = req.session.user
 
         res.redirect('/')
-    })
-  }
-}
-  ,
+      })
+    } else {
+      res.render('users/login', {
+        position: 'position: relative;',
+        errors: errors.mapped(),
+        old: req.body,
+        session: req.session,
+      })
+    }
+  },
+  processRegister: (req, res) => {
+    let errors = validationResult(req)
+
+    if (errors.isEmpty()) {
+      let { firstName, lastName, email, password } = req.body
+      db.User.create({
+        firstName,
+        lastName,
+        email,
+        password: bcrypt.hashSync(password, 12),
+        rolId: 1,
+        avatar: 'default-image.png',
+      })
+        .then(() => {
+          res.redirect('/user/login')
+        })
+        .catch((err) => console.log(err))
+    } else {
+      res.render('users/register', {
+        errors: errors.mapped(),
+        old: req.body,
+        session: req.session,
+        position: 'position: relative',
+      })
+    }
+  },
+
+  logout: (req, res) => {
+    req.session.destroy()
+    if (req.cookies.userLoveLook) {
+      res.cookie('userLoveLook', '', { maxAge: -1 })
+    }
+
+    res.redirect('/')
+  },
      cart: (req, res) => {
         db.Product.findAll({
             where: {
