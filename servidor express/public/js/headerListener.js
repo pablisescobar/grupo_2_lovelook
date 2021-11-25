9cdc9b818b1881bf4eef1b858784e8e1b55c8c49
paloescobar29 <pablodescobarocampo@gmail.coml.com>
2021-11-25 20:31:22 -0300

    home-perfil-demas

--- servidor express/public/js/headerListener.js
+++ servidor express/public/js/headerListener.js
@@ -1,21 +1,8 @@
-window.addEventListener("load", () => {
-    document.getElementById("categoriasDesplegable").addEventListener("mouseover", event => {
-        document.getElementById("categoriasDesplegableView").style.display = 'flex' //elemento a mostrar
-    });
+let desplegar = document.getElementsByClassName('categoryPc')
 
-    document.getElementById("categoriasDesplegable").addEventListener("mouseout", event => {
-        document.getElementById("categoriasDesplegableView").style.display = 'none' //elemento a ocultar
-    });
-    document.getElementById("categorias_dii").addEventListener("click", event => {
-        console.log(event)
-        let categoriasDesplegable = document.getElementById("category_m")
-        if(categoriasDesplegable.style.display == 'none'){
-            categoriasDesplegable.style.display = 'block'
-        }else{
-            categoriasDesplegable.style.display = 'none'
-        }
+let caja = document.getElementById('FrameCategory')
 
-    });
-
-   
+desplegar.addEventListener('click',function(){
+    caja.classList.toggle('active')
+    desplegar.classList.toggle('active')
 })
