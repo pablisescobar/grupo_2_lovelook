0059986887702fa08dfcf14a92f8e210bdb7bebb
Mariela <marielapaduani@gmail.com>
2021-11-29 19:52:44 -0300

    I add dataBase

--- Data Base-Entidad Relación/lovelook_db.sql
+++ Data Base-Entidad Relación/lovelook_db.sql
@@ -75,7 +75,7 @@ CREATE TABLE `images` (
   `filename` varchar(100) NOT NULL,
   `productId` int(11) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=264 DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB AUTO_INCREMENT=271 DEFAULT CHARSET=utf8mb4;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -84,7 +84,7 @@ CREATE TABLE `images` (
 
 LOCK TABLES `images` WRITE;
 /*!40000 ALTER TABLE `images` DISABLE KEYS */;
-INSERT INTO `images` VALUES (186,'1634008479488_img_merida.jpg_.jpg',62),(188,'1634008613548_img_mickey2.jpg_.jpg',64),(190,'1634008855458_img_powerGirls.jpg_.jpg',66),(191,'1634009068632_img_princess.jpg_.jpg',67),(192,'1634009122127_img_remeraBeKind.jpg_.jpg',68),(193,'1634009439465_img_simpsons.jpg_.jpg',69),(194,'1634009490332_img_smile.jpg_.jpg',70),(195,'1634009554779_img_smile2.jpg_.jpg',71),(196,'1634009624778_img_snakes.jpg_.jpg',72),(197,'1634009680926_img_stitch.jpg_.jpg',73),(198,'1634009729159_img_stitch2.jpg_.jpg',74),(199,'1634009799284_img_stitch3.jpg_.jpg',75),(200,'1634009879529_img_tigger.jpg_.jpg',76),(201,'1634009950386_img_tricolor2.jpg_.jpg',77),(202,'1634010008963_img_university.jpg_.jpg',78),(203,'1634010178772_img_university2.jpg_.jpg',79),(204,'1634010343185_img_vogue.jpg_.jpg',80),(205,'1634010427361_img_wild.jpg_.jpg',81),(209,'1634091073674_img_getALife.jpg_.jpg',58),(210,'1634091179974_img_wonderland.jpg_.jpg',82),(211,'1634180349613_img_flower.jpg_.jpg',84),(212,'1634180677217_img_harryPotter.jpg_.jpg',85),(213,'1634180903147_img_line2.jpg_.jpg',86),(219,'1634181345069_img_jean.jpg_.jpg',92),(221,'1634181636179_img_jean9.jpg_.jpg',94),(222,'1634181898795_img_jean2.jpg_.jpg',95),(223,'1634182041340_img_jean4.jpg_.jpg',96),(224,'1634182124096_img_jean6.jpg_.jpg',97),(225,'1634182228282_img_stitch.jpg_.jpg',98),(226,'1634182330034_img_powerGirls.jpg_.jpg',99),(227,'1634182562691_img_harryPotter.jpg_.jpg',100),(228,'1634182684295_img_mickey.jpg_.jpg',101),(230,'1634182887269_img_mickey2.jpg_.jpg',103),(231,'1634182898788_img_kitty.jpg_.jpg',102),(232,'1634182973337_img_selene3.jpg_.jpg',104),(233,'1634183119644_img_mom9.jpg_.jpg',105),(234,'1634183315606_img_mom5.jpg_.jpg',106),(235,'1634183685544_img_powerGirls5.jpg_.jpg',107),(236,'1634183768019_img_hunna2.jpg_.jpg',108),(237,'1634183881789_img_stitch2.jpg_.jpg',109),(238,'1634184051664_img_butterflies.jpg_.jpg',110),(240,'1634184214594_img_angels3.jpg_.jpg',112),(241,'1634184247606_img_doNot3.jpg_.jpg',111),(242,'1634184341991_img_smiles3.jpg_.jpg',113),(243,'1634184424824_img_flower2.jpg_.jpg',114),(244,'1634184495987_img_snakes4.jpg_.jpg',115),(245,'1634184682076_img_babuchaCloe.jpg_.jpg',116),(246,'1634184746031_img_babucha.jpg_.jpg',117),(247,'1634184880150_img_babucha3.jpg_.jpg',118),(248,'1634185019273_img_butterfly.jpg_.jpg',119),(249,'1634185078436_img_changes2.jpg_.jpg',120),(250,'1634185148950_img_kitty.jpg_.jpg',121),(251,'1634185198597_img_playboy3.jpg_.jpg',122),(252,'1634185235748_img_prada3.jpg_.jpg',123),(253,'1634185347648_img_university11.jpg_.jpg',124),(255,'1634507132742_img_RELOG1.webp_.webp',126);
+INSERT INTO `images` VALUES (186,'1634008479488_img_merida.jpg_.jpg',62),(188,'1634008613548_img_mickey2.jpg_.jpg',64),(190,'1634008855458_img_powerGirls.jpg_.jpg',66),(191,'1634009068632_img_princess.jpg_.jpg',67),(192,'1634009122127_img_remeraBeKind.jpg_.jpg',68),(193,'1634009439465_img_simpsons.jpg_.jpg',69),(194,'1634009490332_img_smile.jpg_.jpg',70),(195,'1634009554779_img_smile2.jpg_.jpg',71),(197,'1634009680926_img_stitch.jpg_.jpg',73),(200,'1634009879529_img_tigger.jpg_.jpg',76),(201,'1634009950386_img_tricolor2.jpg_.jpg',77),(202,'1634010008963_img_university.jpg_.jpg',78),(203,'1634010178772_img_university2.jpg_.jpg',79),(204,'1634010343185_img_vogue.jpg_.jpg',80),(205,'1634010427361_img_wild.jpg_.jpg',81),(209,'1634091073674_img_getALife.jpg_.jpg',58),(210,'1634091179974_img_wonderland.jpg_.jpg',82),(211,'1634180349613_img_flower.jpg_.jpg',84),(212,'1634180677217_img_harryPotter.jpg_.jpg',85),(213,'1634180903147_img_line2.jpg_.jpg',86),(219,'1634181345069_img_jean.jpg_.jpg',92),(221,'1634181636179_img_jean9.jpg_.jpg',94),(223,'1634182041340_img_jean4.jpg_.jpg',96),(224,'1634182124096_img_jean6.jpg_.jpg',97),(225,'1634182228282_img_stitch.jpg_.jpg',98),(226,'1634182330034_img_powerGirls.jpg_.jpg',99),(227,'1634182562691_img_harryPotter.jpg_.jpg',100),(228,'1634182684295_img_mickey.jpg_.jpg',101),(230,'1634182887269_img_mickey2.jpg_.jpg',103),(231,'1634182898788_img_kitty.jpg_.jpg',102),(232,'1634182973337_img_selene3.jpg_.jpg',104),(233,'1634183119644_img_mom9.jpg_.jpg',105),(234,'1634183315606_img_mom5.jpg_.jpg',106),(235,'1634183685544_img_powerGirls5.jpg_.jpg',107),(236,'1634183768019_img_hunna2.jpg_.jpg',108),(237,'1634183881789_img_stitch2.jpg_.jpg',109),(238,'1634184051664_img_butterflies.jpg_.jpg',110),(240,'1634184214594_img_angels3.jpg_.jpg',112),(241,'1634184247606_img_doNot3.jpg_.jpg',111),(242,'1634184341991_img_smiles3.jpg_.jpg',113),(243,'1634184424824_img_flower2.jpg_.jpg',114),(244,'1634184495987_img_snakes4.jpg_.jpg',115),(245,'1634184682076_img_babuchaCloe.jpg_.jpg',116),(246,'1634184746031_img_babucha.jpg_.jpg',117),(247,'1634184880150_img_babucha3.jpg_.jpg',118),(248,'1634185019273_img_butterfly.jpg_.jpg',119),(249,'1634185078436_img_changes2.jpg_.jpg',120),(250,'1634185148950_img_kitty.jpg_.jpg',121),(251,'1634185198597_img_playboy3.jpg_.jpg',122),(252,'1634185235748_img_prada3.jpg_.jpg',123),(253,'1634185347648_img_university11.jpg_.jpg',124),(255,'1634507132742_img_RELOG1.webp_.webp',126),(267,'1637802693218_img_university.jpg_.jpg',128),(268,'1637802693224_img_university3.jpg_.jpg',128),(269,'1637802693231_img_university5.jpg_.jpg',128),(270,'1637802693233_img_university7.jpg_.jpg',128);
 /*!40000 ALTER TABLE `images` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -102,7 +102,7 @@ CREATE TABLE `locations` (
   `pc` int(11) DEFAULT NULL,
   `address` varchar(50) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -111,7 +111,7 @@ CREATE TABLE `locations` (
 
 LOCK TABLES `locations` WRITE;
 /*!40000 ALTER TABLE `locations` DISABLE KEYS */;
-INSERT INTO `locations` VALUES (1,'------','------',0,'------'),(2,'------','------',0,'------');
+INSERT INTO `locations` VALUES (1,'------','------',0,'------'),(2,'------','------',0,'------'),(3,'Buenos Aires','Buenos Aires',1663,'Letonia'),(4,'------','------',0,'------'),(5,'Buenos Aires','Buenos Aires',1663,'Letonia');
 /*!40000 ALTER TABLE `locations` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -168,7 +168,7 @@ CREATE TABLE `product_color` (
   KEY `colorId` (`colorId`),
   CONSTRAINT `product_color_ibfk_1` FOREIGN KEY (`productId`) REFERENCES `products` (`id`),
   CONSTRAINT `product_color_ibfk_2` FOREIGN KEY (`colorId`) REFERENCES `colors` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=116 DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB AUTO_INCREMENT=118 DEFAULT CHARSET=utf8mb4;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -177,7 +177,7 @@ CREATE TABLE `product_color` (
 
 LOCK TABLES `product_color` WRITE;
 /*!40000 ALTER TABLE `product_color` DISABLE KEYS */;
-INSERT INTO `product_color` VALUES (47,58,10),(50,61,9),(61,72,1),(63,74,5),(64,75,1),(65,76,6),(66,77,3),(67,78,8),(68,79,3),(69,80,9),(70,81,1),(71,82,2),(73,84,3),(74,85,9),(75,86,8),(81,92,7),(83,94,3),(84,95,7),(85,96,3),(86,97,7),(87,98,7),(88,99,6),(89,100,9),(90,101,3),(91,102,6),(92,103,1),(93,104,3),(94,105,8),(95,106,1),(96,107,6),(97,108,4),(98,109,6),(99,110,5),(100,111,1),(101,112,3),(102,113,2),(103,114,9),(104,115,6),(105,116,3),(106,117,1),(107,118,9),(108,119,1),(109,120,10),(110,121,1),(111,122,2),(112,123,9),(113,124,2),(114,125,6),(115,126,9);
+INSERT INTO `product_color` VALUES (47,58,10),(65,76,6),(66,77,3),(67,78,8),(68,79,3),(69,80,9),(70,81,1),(71,82,2),(73,84,3),(74,85,9),(75,86,8),(81,92,7),(83,94,3),(85,96,3),(86,97,7),(87,98,7),(88,99,6),(89,100,9),(90,101,3),(91,102,6),(92,103,1),(93,104,3),(94,105,8),(95,106,1),(96,107,6),(97,108,4),(98,109,6),(99,110,5),(100,111,1),(101,112,3),(102,113,2),(103,114,9),(104,115,6),(105,116,3),(106,117,1),(107,118,9),(108,119,1),(109,120,10),(110,121,1),(111,122,2),(112,123,9),(113,124,2),(115,126,9),(117,128,2);
 /*!40000 ALTER TABLE `product_color` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -197,7 +197,7 @@ CREATE TABLE `product_size` (
   KEY `sizeId` (`sizeId`),
   CONSTRAINT `product_size_ibfk_1` FOREIGN KEY (`productId`) REFERENCES `products` (`id`),
   CONSTRAINT `product_size_ibfk_2` FOREIGN KEY (`sizeId`) REFERENCES `sizes` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=109 DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB AUTO_INCREMENT=111 DEFAULT CHARSET=utf8mb4;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -206,7 +206,7 @@ CREATE TABLE `product_size` (
 
 LOCK TABLES `product_size` WRITE;
 /*!40000 ALTER TABLE `product_size` DISABLE KEYS */;
-INSERT INTO `product_size` VALUES (46,3,58),(49,1,61),(60,3,72),(62,3,74),(63,4,75),(64,1,76),(65,3,77),(66,3,78),(67,2,79),(68,4,80),(69,1,81),(70,4,82),(72,2,84),(73,3,85),(74,4,86),(75,2,92),(76,4,94),(77,3,95),(78,1,96),(79,2,97),(80,5,98),(81,4,99),(82,3,100),(83,4,101),(84,5,102),(85,3,103),(86,2,104),(87,1,105),(88,3,106),(89,4,107),(90,5,108),(91,3,109),(92,2,110),(93,5,111),(94,2,112),(95,3,113),(96,3,114),(97,2,115),(98,4,116),(99,2,117),(100,2,118),(101,4,119),(102,3,120),(103,2,121),(104,2,122),(105,1,123),(106,1,124),(107,2,125),(108,1,126);
+INSERT INTO `product_size` VALUES (46,3,58),(64,1,76),(65,3,77),(66,3,78),(67,2,79),(68,4,80),(69,1,81),(70,4,82),(72,2,84),(73,3,85),(74,4,86),(75,2,92),(76,4,94),(78,1,96),(79,2,97),(80,5,98),(81,4,99),(82,3,100),(83,4,101),(84,5,102),(85,3,103),(86,2,104),(87,1,105),(88,3,106),(89,4,107),(90,5,108),(91,3,109),(92,2,110),(93,5,111),(94,2,112),(95,3,113),(96,3,114),(97,2,115),(98,4,116),(99,2,117),(100,2,118),(101,4,119),(102,3,120),(103,2,121),(104,2,122),(105,1,123),(106,1,124),(108,1,126),(110,4,128);
 /*!40000 ALTER TABLE `product_size` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -231,7 +231,7 @@ CREATE TABLE `products` (
   KEY `seasonId` (`seasonId`),
   CONSTRAINT `products_ibfk_1` FOREIGN KEY (`categoryId`) REFERENCES `categories` (`id`),
   CONSTRAINT `products_ibfk_2` FOREIGN KEY (`seasonId`) REFERENCES `seasons` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=127 DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB AUTO_INCREMENT=129 DEFAULT CHARSET=utf8mb4;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -240,7 +240,7 @@ CREATE TABLE `products` (
 
 LOCK TABLES `products` WRITE;
 /*!40000 ALTER TABLE `products` DISABLE KEYS */;
-INSERT INTO `products` VALUES (58,'Get a Life','Remeron de algodÃ³n estampado',1200,11,3,3,0),(61,'Looney Tunes','Remeron de algodÃ³n estampado',1200,14,3,2,15),(72,'Snakes','Remeron de algodÃ³n con diseÃ±o estampado',1200,25,3,2,0),(74,'Stitch','Remeron de algodÃ³n con diseÃ±o de stitch estampado',1200,12,3,3,0),(75,'Stitch','Remeron de algodÃ³n con diseÃ±o de stitch estampado',1500,9,3,3,0),(76,'Tigger','Remeron de algodÃ³n estampado',1320,11,3,2,0),(77,'Tricolor','Conjunto de buzo y babucha frizado',4500,9,4,1,25),(78,'University','Buzo frizado con diseÃ±o universitario',2400,4,1,4,15),(79,'University','Buzo frizado con diseÃ±o universitario',2400,6,1,1,15),(80,'Vogue','Maxibuzo frizado con diseÃ±o estampado',2600,6,5,1,15),(81,'Wilds','Remera de algodÃ³n con detalle en las vistas',1000,7,2,2,0),(82,'Wonderland','Remeron de algodÃ³n con diseÃ±o de dibujos estampado',1200,8,3,3,0),(84,'Flower','Buzo frizado con diseÃ±o estampado',2400,15,1,1,15),(85,'Harry Potter','Buzo frizado ediciÃ³n exclusiva de Harry Potter',2400,21,1,1,15),(86,'Line II','Buzo de algodÃ³n rÃºstico con combinaciones',2300,26,1,3,0),(92,'Jean Slouchy','PantalÃ³n de jean ',1800,12,8,3,0),(94,'Jean Skinny','PantalÃ³n de jean elastizado con roturas',2000,13,8,3,0),(95,'Jean Mom','PantalÃ³n de jean',2000,11,8,3,0),(96,'Rebels','Jardinero de jean',2200,9,8,4,15),(97,'wide II','PantalÃ³n de jean amplio',2200,10,8,4,15),(98,'Stitch','Remera y short con diseÃ±o sublimado',2500,22,9,2,0),(99,'PowerPuff','Remera y short con diseÃ±o sublimado',2500,21,9,2,0),(100,'Harry Potter','Remera manga larga y pantalÃ³n babucha con diseÃ±o sublimado',2500,14,9,4,15),(101,'Mickey','Remera manga larga y pantalÃ³n babucha con diseÃ±o estampado',2500,13,9,4,15),(102,'Hello Kitty','Campera de algodÃ³n rÃºstico con diseÃ±o estampado',2800,23,6,3,0),(103,'Mickey','Campera de algodÃ³n rÃºstico con diseÃ±o estampado',2800,23,6,3,0),(104,'Selene','Campera bÃ¡sica de algodÃ³n rÃºstico',2600,16,6,3,0),(105,'Mom II','Campera larga frizada, capucha con corderito',2800,12,6,1,15),(106,'Changes','Campera larga de algodÃ³n rÃºstico',2800,12,6,3,0),(107,'Power Girls','Maxibuzo con diseÃ±o estampado',2600,13,5,3,0),(108,'Hunna','Maxibuzo con diseÃ±o bordado',2600,18,5,3,0),(109,'Stitch','Maxibuzo de algodÃ³n rÃºstico con diseÃ±o sublimado',2600,16,5,3,0),(110,'Butterflies','Conjunto de buzo y babucha con diseÃ±os estampados',4500,9,4,1,25),(111,'Do Not','Conjunto de buzo y babucha con diseÃ±os',4500,7,4,1,25),(112,'Angels','Conjunto frizado de buzo y babucha con diseÃ±o estampado',4500,6,4,1,25),(113,'Smile','Conjunto frizado de buzo y babucha',4500,8,4,1,25),(114,'Flowers','Conjunto frizado de buzo y babucha',4500,9,4,1,25),(115,'Snakes','Conjunto frizado de buzo y babucha con diseÃ±o estampado',4500,12,4,1,25),(116,'Slipper','Babucha de algodÃ³n frizado',1800,11,7,1,15),(117,'Slipper II','Babucha de algodÃ³n frizado',1800,13,7,1,15),(118,'Cloe II','Babucha de algodÃ³n rÃºstico',1800,13,7,4,0),(119,'Butterfly','RemerÃ³n de algodÃ³n con diseÃ±o de mariposa',1200,13,3,2,0),(120,'Changes','RemerÃ³n de algodÃ³n con diseÃ±o',1200,15,3,2,0),(121,'Kitty','RemerÃ³n con diseÃ±o de Hello Kitty',1200,18,3,2,0),(122,'Playboy','RemerÃ³n con diseÃ±o estampado',1200,18,3,2,0),(123,'Prada','RemerÃ³n con diseÃ±o estampado',1200,19,3,2,0),(124,'University','RemerÃ³n con diseÃ±o ',1200,16,3,2,0),(125,'Life II','Remera con diseÃ±o estampado',1000,19,2,2,0),(126,'Reloj Casio Mujer Ltp-1131g','· HORA REGULAR Analógico: Tres manillas (Horas, minutos, segundos)\r\n· PRECISION: +/- 20 Seg.. por me',10000,50,12,5,25);
+INSERT INTO `products` VALUES (58,'Get a Life','Remerón de algodón estampado',1200,11,3,3,0),(76,'Tigger','Remerón de algodón estampado',1320,11,3,2,0),(77,'Tricolor','Conjunto de buzo y babucha frizado',4500,9,4,1,25),(78,'University','Buzo frizado con diseño universitario',2400,4,1,4,15),(79,'University','Buzo frizado con diseño universitario',2400,6,1,1,15),(80,'Vogue','Maxibuzo frizado con diseño estampado',2600,6,5,1,15),(81,'Wilds','Remera de algodón con detalle en las vistas',1000,7,2,2,0),(82,'Wonderland','Remerón de algodón con diseño de dibujos estampado',1200,8,3,3,0),(84,'Flower','Buzo frizado con diseño estampado',2400,15,1,1,15),(85,'Harry Potter','Buzo frizado edición exclusiva de Harry Potter',2400,21,1,1,15),(86,'Line II','Buzo de algodón rústico con combinaciones',2300,26,1,3,0),(92,'Jean Slouchy','Pantalón de jean con puño entallado',1800,12,8,3,0),(94,'Jean Skinny','Pantalón de jean elastizado con roturas',2000,13,8,3,0),(96,'Rebels','Jardinero de jean',2200,9,8,4,15),(97,'wide II','Pantalón de jean amplio',2200,10,8,4,15),(98,'Stitch','Remera y short con diseño sublimado',2500,22,9,2,0),(99,'PowerPuff','Remera y short con diseño sublimado',2500,21,9,2,0),(100,'Harry Potter','Remera manga larga y pantalón babucha con diseño sublimado',2500,14,9,4,15),(101,'Mickey','Remera manga larga y pantalón babucha con diseño estampado',2500,13,9,4,15),(102,'Hello Kitty','Campera de algodón rústico con diseño estampado',2800,23,6,3,0),(103,'Mickey','Campera de algodón rústico con diseño estampado',2800,23,6,3,0),(104,'Selene','Campera básica de algodón rústico',2600,16,6,3,0),(105,'Mom II','Campera larga frizada, capucha con corderito',2800,12,6,1,15),(106,'Changes','Campera larga de algodón rústico',2800,12,6,3,0),(107,'Power Girls','Maxibuzo con diseño estampado',2600,13,5,3,0),(108,'Hunna','Maxibuzo con diseño bordado',2600,18,5,3,0),(109,'Stitch','Maxibuzo de algodón rústico con diseño sublimado',2600,16,5,3,0),(110,'Butterflies','Conjunto de buzo y babucha con diseños estampados',4500,9,4,1,25),(111,'Do Not','Conjunto de buzo y babucha con diseÃ±os',4500,7,4,1,25),(112,'Angels','Conjunto frizado de buzo y babucha con diseño estampado',4500,6,4,1,25),(113,'Smile','Conjunto frizado de buzo y babucha',4500,8,4,1,25),(114,'Flowers','Conjunto frizado de buzo y babucha',4500,9,4,1,25),(115,'Snakes','Conjunto frizado de buzo y babucha con diseño estampado',4500,12,4,1,25),(116,'Slipper','Babucha de algodón frizado',1800,11,7,1,15),(117,'Slipper II','Babucha de algodón frizado',1800,13,7,1,15),(118,'Cloe II','Babucha de algodón rústico',1800,13,7,4,0),(119,'Butterfly','Remerón de algodón con diseño de mariposa',1200,13,3,2,0),(120,'Changes','Remerón de algodón con diseño',1200,15,3,2,0),(121,'Kitty','Remerón con diseño de Hello Kitty',1200,18,3,2,0),(122,'Playboy','Remerón con diseño estampado',1200,18,3,2,0),(123,'Prada','Remerón con diseño estampado',1200,19,3,2,0),(124,'University','Remerón con diseño estampado',1200,16,3,2,0),(126,'Reloj Casio Mujer Ltp-1131g','· HORA REGULAR Analógico: Tres manillas (Horas, minutos, segundos)\r\n· PRECISION: +/- 20 Seg.. por me',10000,50,12,5,25),(128,'Disney','Remera de mujer con detalles estampados',1200,14,2,2,0);
 /*!40000 ALTER TABLE `products` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -255,7 +255,7 @@ CREATE TABLE `roles` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(50) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -264,7 +264,7 @@ CREATE TABLE `roles` (
 
 LOCK TABLES `roles` WRITE;
 /*!40000 ALTER TABLE `roles` DISABLE KEYS */;
-INSERT INTO `roles` VALUES (1,'USER'),(2,'ADMIN');
+INSERT INTO `roles` VALUES (1,'USER'),(2,'ADMIN'),(3,'SUPERUSER');
 /*!40000 ALTER TABLE `roles` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -399,7 +399,7 @@ CREATE TABLE `users` (
   KEY `rolId` (`rolId`),
   CONSTRAINT `users_ibfk_1` FOREIGN KEY (`locationId`) REFERENCES `locations` (`id`),
   CONSTRAINT `users_ibfk_3` FOREIGN KEY (`rolId`) REFERENCES `roles` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -408,7 +408,7 @@ CREATE TABLE `users` (
 
 LOCK TABLES `users` WRITE;
 /*!40000 ALTER TABLE `users` DISABLE KEYS */;
-INSERT INTO `users` VALUES (5,'mariela','paduani',NULL,'mariela@gmail.com','$2a$12$tD5e.5Jhp/WQWMgAH8njjeREM6PBt7F9w7jlZfk6Mc1VBcHCH5V.O',2,NULL,'1637200594272_img_.jpg');
+INSERT INTO `users` VALUES (5,'mariela','paduani',NULL,'mariela@gmail.com','$2a$12$tD5e.5Jhp/WQWMgAH8njjeREM6PBt7F9w7jlZfk6Mc1VBcHCH5V.O',3,NULL,'1637200594272_img_.jpg'),(7,'Pablo','Escobar',2147483647,'ocampo_pablo@live.com','$2a$12$69UkLoL060zoovCdAnDmLO9St9FlCoXUeng.5t03C3AZJ7nqZBBQu',1,NULL,'1637714499227_img_.jpg'),(9,'marye','paduani',NULL,'marye@gmail.com','$2a$12$dTHRco.lAYRa5/JcPDFHq.aqiHKVYgGOsth8NE5LnxaglXiMX4pUa',2,NULL,'default-image.png');
 /*!40000 ALTER TABLE `users` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -425,4 +425,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2021-11-18  0:20:19
+-- Dump completed on 2021-11-26  1:20:30
