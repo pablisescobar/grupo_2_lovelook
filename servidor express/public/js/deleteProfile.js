12da82d76649aa8a2cf3950ec133a4ec377f9236
paloescobar29 <pablodescobarocampo@gmail.coml.com>
2021-12-01 18:56:39 -0300

    cambio de imagen al header y parcialmente apis

--- servidor express/public/js/deleteProfile.js
+++ servidor express/public/js/deleteProfile.js
@@ -1,22 +1,37 @@
-function global (element) {
-    return document.querySelector(element)
+function global(element) {
+  return document.querySelector(element);
 }
 
-
-window.addEventListener("load", function(){
-    let buttonDel = global("#bad-btn-submit")
-    let formDel = global("#formDelete")
-    
-    buttonDel.addEventListener("click", function(){
-        let opcion = confirm("¿Estas seguro de querer eliminar tu cuenta?, está accion no se puede revertir")
-        
-        formDel.addEventListener("submit", function(event){
-            event.preventDefault()
-
-            if(opcion === true){
-                formDel.submit()
-            } 
-        })
-    })
-
-})
+window.addEventListener("load", function () {
+  let formDel = global("#formDelete");
+  formDel.addEventListener("submit", function (event) {
+    event.preventDefault();
+    const swalWithBootstrapButtons = Swal.mixin({
+      customClass: {
+        confirmButton: "swal-confirm",
+        cancelButton: "swal-cancel",
+      },
+      buttonsStyling: false,
+    });
+    swalWithBootstrapButtons
+      .fire({
+        title: "¿Estas seguro de vaciar el carrito?",
+        icon: "question",
+        showCancelButton: true,
+        confirmButtonText: "Confirmar",
+        cancelButtonText: "Cancelar",
+        reverseButtons: true,
+      })
+      .then((result) => {
+        if (result.isConfirmed) {
+          swalWithBootstrapButtons.fire({
+            title: "Usuario eliminado",
+            icon: "success",
+            timer: 1000,
+            showConfirmButton: false,
+          });
+          formDel.submit();
+        }
+      });
+  });
+});
