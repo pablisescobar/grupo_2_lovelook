1cd9149976f1437056139477552d6e4a321050d7
Mariela <marielapaduani@gmail.com>
2021-12-03 17:29:53 -0300

    I addI upload the image preview and fixes

--- servidor express/src/middleware/uploadAdminCheck.js
+++ servidor express/src/middleware/uploadAdminCheck.js
@@ -1,5 +1,7 @@
 module.exports = (req,res,next)=>{
-    if(req.session.user && req.session.user.rol === 2 || req.session.user.rol === 3){
+    if(req.session.user && req.session.user.rol === 2){
+        next()
+    } else if (req.session.user && req.session.user.rol === 3) {
         next()
     }else{
         res.redirect('/')
